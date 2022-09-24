496af192f4fe759ec3f111a5bbdb9aacfd57e107
Rashmi-N97 <nrashmi097@gmail.com>
2022-09-24 04:31:19 -0500

    added 3 new models

--- datamodel.py
+++ datamodel.py
@@ -2,3 +2,13 @@
 
 
 print("this is a code for data modelling")
+
+
+def model1():
+     print("add code for ML model 2")
+
+def model2():
+    print("add code for ML model 2")
+
+def model2():
+    print("add code for ML model 3")
